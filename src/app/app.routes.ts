c070bb83060e2f0eb6fff72bfaf5bd775a32a809
Annelie Svensson <annelievs@hotmail.com>
2024-05-16 15:25:57 +0200

    routes-justeringar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,10 @@ import { ConvertComponent } from './convert/convert.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'about', component: AboutComponent},
-    { path: 'convert', component: ConvertComponent},
+    { path: '/home', component: HomeComponent},
+    { path: '/about', component: AboutComponent},
+    { path: '/convert', component: ConvertComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '**', component: NotFoundComponent},
-    {path: '404', component: NotFoundComponent},
+    {path: '/404', component: NotFoundComponent},
 ];
